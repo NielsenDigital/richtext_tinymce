507ea664af510417d1f9c6cf896287230e1be680
Will Nielsen <will@nielsendigital.com>
2013-03-26 11:45:36 -0400

    removed pastes that weren't for Word. Removed H1, H2 and address from format select menu

--- assets/richtext_tinymce.publish.js
+++ assets/richtext_tinymce.publish.js
@@ -11,13 +11,14 @@ jQuery(document).ready(function() {
 		script_url: Symphony.Context.get('root') + '/extensions/richtext_tinymce/lib/tiny_mce.js',
 
 		theme: 'advanced',
-		plugins: '',
+		plugins: 'paste',
 		skin: 'symphony',
 
-		theme_advanced_buttons1 : 'formatselect,bold,italic,underline,|,bullist,numlist,outdent,indent,|,blockquote,link,unlink,image,|,copy,paste,pastetext,pasteword,|,undo,redo,|,cleanup,code',
+		theme_advanced_buttons1 : 'formatselect,bold,italic,underline,|,bullist,numlist,outdent,indent,|,blockquote,link,unlink,image,|,copy,pasteword,|,undo,redo,|,cleanup,code',
 		theme_advanced_buttons2 : '',
 		theme_advanced_buttons3 : '',
 		theme_advanced_buttons4 : '',
+		theme_advanced_blockformats : 'p,h3,h4,h5,h6,pre',
 		theme_advanced_toolbar_location : 'top',
 		theme_advanced_toolbar_align : 'left',
 		theme_advanced_resizing : true,
